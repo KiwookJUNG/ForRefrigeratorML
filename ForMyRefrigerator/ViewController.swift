b983317ac9dada8d14d3e6a3847bd572af58ae23
정기욱 <rldnr56@me.com>
2019-04-09 20:18:31 +0900

    Initial Commit

---
+++ ForMyRefrigerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForMyRefrigerator
+//
+//  Created by 정기욱 on 09/04/2019.
+//  Copyright © 2019 Nudge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
