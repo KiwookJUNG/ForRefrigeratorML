009bbe054ca43383a7cc782fbd1245738c4ac220
정기욱 <rldnr56@me.com>
2019-05-12 03:25:39 +0900

    Custom Ingredient View Cell

---
+++ ForMyRefrigerator/IngrdedientCell.swift
@@ -0,0 +1,17 @@
+//
+//  IngrdedientCell.swift
+//  ForMyRefrigerator
+//
+//  Created by 정기욱 on 09/05/2019.
+//  Copyright © 2019 Nudge. All rights reserved.
+//
+
+import UIKit
+
+class IngredientCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var ingrdient: UILabel!
+    
+    @IBOutlet weak var want: UISwitch!
+}
