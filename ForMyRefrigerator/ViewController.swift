dcb8890ff3a79ed90d641ce7fea2bf92bf96fa22
정기욱 <rldnr56@me.com>
2019-05-12 03:15:52 +0900

    AVCapture와 MLModel을 사용한 멀티스레드 실시간 객체 인식

--- ForMyRefrigerator/ViewController.swift
+++ ForMyRefrigerator/ViewController.swift
@@ -14,16 +14,49 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
 
     // Methods for receiving sample buffers from and monitoring the status of a video data output.
     // AVCaputreVideoDataOutputSampleBufferDelegate는 샘플 버퍼를 위한 메소드와 비디오 데이터 아웃풋의 상태를 모니터링함.
-    
+    var rootLayer: CALayer! = nil
     @IBOutlet weak var object: UILabel!
     @IBOutlet weak var confidence: UILabel!
+    @IBOutlet weak var viewForCamera: UIView!
+    @IBOutlet weak var rectView: UIView!
+    
+    @IBOutlet weak var output: UILabel!
+    var hasIngredient : [String] = []
+    var Str = ""
+    
+    var ingredient : [String : String] = ["Onion":"양파", "Egg":"달걀", "Green Onion":"대파", "Hairtail":"갈치", "Kimchi":"김치", "Mackerel" :"고등어", "Meat":"소고기", "Milk":"우유", "Pork":"돼지고기", "Red Pepper Paste": "고추장", "Soybean":"된장", "Tofu":"두부"]
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        // 카메라 시작
         
+        self.object.layer.masksToBounds = true
+        self.object.layer.cornerRadius = 5
+        self.confidence.layer.masksToBounds = true
+        self.confidence.layer.cornerRadius = 5
+        self.output.text = ""
+        // Do any additional setup after loading the view, typically from a nib.
+      
+        // 네비게이션 타이틀의 색을 바꿔줌
+        let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
+        navigationController?.navigationBar.titleTextAttributes = textAttributes
+        
+        // 네비게이션 바의 배경을 바꿔줌.
+        self.navigationController?.navigationBar.barTintColor = UIColor(red:0.14, green:0.35, blue:0.91, alpha:1.0)
+        
+        self.rectView.layer.borderColor = UIColor(red:0.67, green:0.71, blue:0.80, alpha:1.0).cgColor
+        self.rectView.layer.borderWidth = 2
+        self.rectView.layer.masksToBounds = true
+        self.rectView.layer.cornerRadius = 5
+        
+        
+        
+        // 카메라 시작
         // 카메라 동영상을 위한 AVCaptureSession 인스턴스 생성
+        // An object that manages capture activity and coordinates the flow of data from input devices to capture outputs.
         let captureSession = AVCaptureSession()
         // 사진 수준의 퀄리티를 전송한다.
         captureSession.sessionPreset = .photo
@@ -43,8 +76,15 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         
         // 캡쳐하고 있는 비디오 화면을 보여주는 Vedio레이어
         let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        view.layer.addSublayer(previewLayer)
-        previewLayer.frame = view.frame
+        
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        rootLayer = viewForCamera.layer
+        previewLayer.frame = rootLayer.bounds
+        
+        previewLayer.addSublayer(rectView.layer)
+        rectView.frame.origin.x = 40
+        rectView.frame.origin.y = 40
+        rootLayer.addSublayer(previewLayer)
         
 
         // A capture output that records video and provides access to video frames for processing.
@@ -63,7 +103,6 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     // Notifies the delegate that a new video frame was written.
     // 델리게이트에게 새로운 비디오 프레임이 찍고 있는 것을 알려주고 그때 실시하는 메소드
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-//        print("카메라가 실시간으로 찍고 있고 videoQueue에 저장되고 있습니다.", Date())
         
         // Returns a sample buffer's CVImageBuffer of media data.
         // 매개변수로 받은 샘플버퍼를 리턴해준다.
@@ -72,12 +111,10 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         }
         
         // 사용할 MLModel을 결정해준다.
-        
         guard let model = try? VNCoreMLModel(for: Advanced().model) else { return }
         
         
         //An image analysis request that uses a Core ML model to process images.
-        
         // ML model을 사용한 이미지의 분석 요청
         let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
             // 에러 체크
@@ -88,18 +125,47 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             
             guard let firstObservation = results.first else { return }
             
+        
+            
             DispatchQueue.main.async { // Correct
-                self.object.text = "물체 : \(firstObservation.identifier)"
-                self.confidence.text = "정확도 : \(firstObservation.confidence)"
+                
+//                if ( self.hasIngredient.count >= 1){
+//
+//                }
+                
+                if (firstObservation.confidence > 0.61)
+                {
+                    if( !self.hasIngredient.contains(String(firstObservation.identifier)))
+                    {
+                        self.hasIngredient.append(firstObservation.identifier)
+                        self.Str = self.Str + "\(self.ingredient[firstObservation.identifier]!) "
+                    }
+                }
+                
+                self.object.text = "물체 : \(self.ingredient[firstObservation.identifier]!)"
+                self.confidence.text = "정확도 : \(round(firstObservation.confidence*100)/100)"
+                //round(avgTemp*100)/100
+                
+                
+                self.output.text = self.Str
+                
+                
             }
             
             
         }
-        
         // 이미지 분석 요청을 Array로 받아 관리한다.
         try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [ : ]).perform([request])
     }
   
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "IngredientSegue" {
+            
+            let IngredientInfo = segue.destination as? DetailViewController
+            IngredientInfo?.hasIngredient = self.hasIngredient
+        }
+    }
 
 }
 
