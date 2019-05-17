0b042167571ca73a0738b1dc202e6420a1df837b
정기욱 <rldnr56@me.com>
2019-05-17 15:45:52 +0900

    Collection View 를 위한 Cell Class

---
+++ ForMyRefrigerator/Refreigerator/CollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionViewCell.swift
+//  ForMyRefrigerator
+//
+//  Created by 정기욱 on 11/05/2019.
+//  Copyright © 2019 Nudge. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    
+    
+    @IBOutlet weak var foodImage: UIImageView!
+    
+    @IBOutlet weak var foodName: UILabel!
+    
+    
+}
