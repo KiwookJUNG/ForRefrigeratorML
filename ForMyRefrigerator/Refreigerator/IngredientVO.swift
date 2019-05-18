b65fc62ac1a28c17a3c9ffead4a228bec46765ac
KiwookJUNG <ismeeki17@naver.com>
2019-05-19 01:45:52 +0900

    MVC모델을 사용하기 위해 Ingredient Value Object를 따로 만들어줬다.

---
+++ ForMyRefrigerator/Refreigerator/IngredientVO.swift
@@ -0,0 +1,39 @@
+//
+//  IngredientVO.swift
+//  ForMyRefrigerator
+//
+//  Created by 정기욱 on 19/05/2019.
+//  Copyright © 2019 Nudge. All rights reserved.
+//
+
+import Foundation
+
+class IngredientVO {
+    
+ 
+        var ingredient : [String : String] = ["Onion":"양파",
+                                              "Egg":"달걀",
+                                              "Green Onion":"대파",
+                                              "Hairtail":"갈치",
+                                              "Kimchi":"김치",
+                                              "Mackerel" :"고등어",
+                                              "Meat":"소고기",
+                                              "Milk":"우유",
+                                              "Pork":"돼지고기",
+                                              "Red Pepper Paste": "고추장",
+                                              "Soybean":"된장",
+                                              "Tofu":"두부",
+                                              "Tteok":"떡",
+                                              "Cabbage":"양배추",
+                                              "Carrot":"당근",
+                                              "Chili":"고추",
+                                              "Crushed Garlic":"다진 마늘",
+                                              "Fish Cake":"어묵",
+                                              "Green Pumpkin":"애호박",
+                                              "Ham":"햄",
+                                              "Manila Calm":"바지락",
+                                              "Sausage":"소세지",
+                                              "Garlic":"마늘",
+                                              "Chicken":"닭고기"]
+    
+}
