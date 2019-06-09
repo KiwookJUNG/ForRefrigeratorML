0133d5de5a1658d69e401f4914468ce574fe11ac
KiwookJUNG <ismeeki17@naver.com>
2019-06-09 18:07:58 +0900

    레시피 뷰 셀의 클래스

---
+++ ForMyRefrigerator/Refreigerator/스캔(classifier)/RecipeCell.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeCell.swift
+//  ForMyRefrigerator
+//
+//  Created by 정기욱 on 03/06/2019.
+//  Copyright © 2019 Nudge. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var direction: UILabel!
+    
+    @IBOutlet weak var directionImage: UIImageView!
+    
+}
