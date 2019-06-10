a6f894949d621402b336b2031ca526aab5fd0e9c
KiwookJUNG <ismeeki17@naver.com>
2019-06-10 14:21:19 +0900

    Today Food View Controller에서 사용되는 Custom Cell

---
+++ ForMyRefrigerator/Refreigerator/오늘의 레시피/TodayFoodCell.swift
@@ -0,0 +1,20 @@
+//
+//  TodayFoodCell.swift
+//  ForMyRefrigerator
+//
+//  Created by 정기욱 on 02/06/2019.
+//  Copyright © 2019 Nudge. All rights reserved.
+//
+
+import UIKit
+
+class TodayFoodCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var todayFoodImage: UIImageView!
+    
+    @IBOutlet weak var todayFoodName: UILabel!
+    
+    @IBOutlet weak var todayFoodCounter: UILabel!
+    
+}
